9332a2392e6e4b2df46f0684c9d8097388bcc25c
Taiki <tyoshikawa1106@gmail.com>
2015-08-19 20:31:29 +0900

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JsforceSampleApp::Application.config.secret_key_base = 'd9b7e254d1d64c77145a287baf16b759bf8ea1e8ad4137983604ff37dd14eb9001cb9dc54295febb569f89c9d72236616a0831d40c213571e75630bd27d97f42'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JsforceSampleApp::Application.config.session_store :cookie_store, key: '_jsforce_sample_app_session'
