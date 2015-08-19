7b71519a5262f2d9073b2c7f64bdef1d65423264
Taiki <tyoshikawa1106@gmail.com>
2015-08-19 21:39:41 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 JsforceSampleApp::Application.routes.draw do
+  match '/connect', to: 'pages#connect', via: 'get'
+  root "pages#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
