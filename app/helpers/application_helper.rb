cfb35d0fb22c6928b7dadf87d287b15cc62b4539
Taiki <tyoshikawa1106@gmail.com>
2015-08-24 04:22:55 +0900

    commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Get title
+  def get_title(page_title)
+    base_title = "JSforce Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title}"
+    end
+  end
 end
